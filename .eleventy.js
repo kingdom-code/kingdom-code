a48a3a7e53ea10d77c3f6a8ff7eab12956ca3ae2
James Doc <hi@jamesdoc.com>
2019-04-07 22:24:47 +0100

    Add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("isFutureDate", (dateObj)=> {
     return DateTime.fromISO(dateObj) > DateTime.local()
-  })
+  });
+
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
 
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromISO(dateObj).toFormat("dd LLLL yyyy");
