ac7f811e6f4ad967d30a2505e8e469c2ed076dce
James Doc <hi@jamesdoc.com>
2019-06-05 19:09:43 +0100

    Add in quote block\

--- src/_assets/_js/script.js
+++ src/_assets/_js/script.js
@@ -1,4 +1,5 @@
 import MoveTo from 'moveto';
+import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider"
 
 document.addEventListener("DOMContentLoaded", function(){
   const moveTo = new MoveTo();
@@ -7,3 +8,19 @@ document.addEventListener("DOMContentLoaded", function(){
     moveTo.registerTrigger(el);
   });
 });
+
+const slider = document.querySelector('.js-slider');
+if (slider !== null) {
+  tns({
+    container: '.slider__container',
+    items: 1,
+    autoHeight: false,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    autoplayTimeout: 3500,
+    mode: 'gallery',
+    speed: 400,
+    controls: false,
+    nav: false
+  });
+}
