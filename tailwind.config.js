e3ae3466c0d1a00e1ec068f0b2e48ea0353db760
James Doc <hi@jamesdoc.com>
2022-01-09 12:01:31 +0000

    Add some tailwind styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.njk", "./src/**/*.html"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      orange: "#ee5d52",
+      mint: "#1cac9c",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 let mix = require("laravel-mix");
 
 mix.sass("src/_assets/scss/style.scss", "./dist/_assets/css");
+mix.postCss("src/_assets/scss/tw.css", "./dist/_assets/css", [
+  require("tailwindcss"),
+]);
 mix.js("src/_assets/js/script.js", "./dist/_assets/js");
 
 mix.disableNotifications();
