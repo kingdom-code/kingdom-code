a29afa379ecfdab55d822c60ffdbfcd4145d8567
James Doc <hi@jamesdoc.com>
2022-02-01 21:20:36 +0000

    Cosmetic to jobs and nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       white: "#ffffff",
       orange: "#ee5d52",
       mint: "#1cac9c",
-      blue: "#4e5569"
+      blue: "#4e5569",
+      grey: "#b7b8be"
     },
     extend: {},
   },
